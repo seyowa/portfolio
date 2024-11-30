054ec99d70bf7d5c10831d85ee796146e488b8a2
seyeomowa <omowa.oluwaseye@yahoo.com>
2024-11-30 14:49:00 +0200

    homepage/modified using map to render json data

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -4,16 +4,19 @@ import { replacePlaceholders } from '../utils/replacePlaceholders';
 const Home = ({data}) => {
   return (
     <section id='home' className='my-10' >
-      <div mt-2>
-        <p className='text-xl'>{replacePlaceholders(data.home?.greetings)}</p>
-          <p className='text-4xl font-semibold mt-2'>{replacePlaceholders(data.home?.intro.job)}</p>
-          <p className='text-xl'>{replacePlaceholders(data.home?.intro.headline)}</p>
-          <p className='text-xl'>{replacePlaceholders(data.home?.intro.moreheadline)}</p>
+      <div className='text-xl'>{replacePlaceholders(data.home?.greetings)}</div>
+      <div>
+        {data.home?.items?.map((h, index) => (
+          <div key={index}>
+            <p className='text-4xl font-semibold mt-2'>{h.occupation}</p>
+            <p className='text-xl'>{h.headline}<br/>
+            {h.headlineContinues}</p>
 
-
+          </div>
+        ))}
       </div>
-{/*          */}
-    </section>
+
+     </section>
   );
 };
 
--- my-portfolio/src/utils/replacePlaceholders.js
+++ my-portfolio/src/utils/replacePlaceholders.js
@@ -5,9 +5,6 @@ export const replacePlaceholders = (text) => {
     return text
       .replace('[Firstname]', 'Seye')
       .replace('[Lastname]', 'Omowa')
-      .replace('[My Occupation]', 'Web Developer')
-      .replace('[Headline goes here]', 'Eager to bring ideas to life')
-      .replace('[Headline Continues here]', 'Let\'s create something amazing!')
       .replace('[Current Year]', currentYear)
       .replace('[Your detailed bio goes here]', 'I am a passionate web developer with a knack for creating stunning web experiences.')
       .replace('[Detailed descriptions of your projects go here]', 'Here are some of the projects I have worked on.')
