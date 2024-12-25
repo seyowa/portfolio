4b32615cfd1b9ab3fa5f5d4fc24bfd1ab779937e
seyowa <seye.omowa@proton.me>
2024-12-26 00:49:42 +0200

    feat(SEO): update meta tags and titles
    - configure dynamic content
    
    Fix #44

---
+++ my-portfolio/src/components/SEO.js
@@ -0,0 +1,16 @@
+import { Helmet } from 'react-helmet-async';
+import content from '../content.json';
+
+const SEO = () => (
+  <Helmet>
+    <title>{content.user?.name || "Seye's"} | Portfolio</title>
+    <meta
+      name="description"
+      content={content.user?.description || "Web Developer Portfolio"}
+    />
+    <meta name="keywords" content={content.keywords || "web developer, software engineer"} />
+    <meta name="author" content={content.user?.name} />
+  </Helmet>
+);g
+
+export default SEO;
