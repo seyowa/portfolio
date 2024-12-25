7dd48f4296bc7d4fc79d16439587049909c4811d
seyowa <seye.omowa@proton.me>
2024-12-26 00:35:43 +0200

    fix(logo): fix route problem
    - fix route to update to home when logo is clicked
    - update from using href to Link
    - add useNavigate to update route
    
    Related to #1

--- my-portfolio/src/components/Logo.js
+++ my-portfolio/src/components/Logo.js
@@ -1,22 +1,27 @@
 import React from 'react';
-
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const Logo = () => {
+  const navigate = useNavigate();
+
   const scrollToTop = (e) => {
     e.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    navigate('/');
   };
+  
 
   return (
-    <a 
-      href="/" onClick={scrollToTop}
+    <Link 
+      to="/" onClick={scrollToTop}
       className="flex items-center"
+
     >
       <span className="text-xl font-bold text-gray-800 hover:text-blue-600 transition-colors duration-300">
         SO
       </span>
-    </a>
+    </Link>
   );
 };
 
