a61b084c7dadacd089c643a84c723c48567aa087
seyowa <seye.omowa@proton.me>
2025-01-02 15:11:11 +0200

    chore(home): update background color and call-to-action button color

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -3,25 +3,22 @@ import { Link } from 'react-router-dom';
 import content from '../content.json';
 import { motion } from 'framer-motion';
 
-
 const Home = () => {
-  let userName = "Seye";
-  let userKeywords = ["developer"];
+  let userName = 'Seye';
+  let userKeywords = ['developer'];
 
   try {
-    userName = content?.user?.name || "Seye";
-    userKeywords = content?.keywords || ["developer"];
+    userName = content?.user?.name || 'Seye';
+    userKeywords = content?.keywords || ['developer'];
   } catch (error) {
-    console.error("Error loading contents:", error);
+    console.error('Error loading contents:', error);
   }
   return (
     <motion.section
-      className="flex h-screen items-center justify-center bg-gradient-to-br from-gray-900 to-blue-900 text-white"
-
+      className="flex h-screen items-center justify-center bg-gradient-to-r from-gray-500 to-gray-800 text-white"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
-
     >
       <div className="text-center">
         <motion.h1
@@ -38,9 +35,13 @@ const Home = () => {
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1, delay: 1 }}
         >
-          A passionate <span className="capitalize">{userKeywords[1]}</span><br />
-          <span className="mt-2 block italic">Eager to bring ideas to life<br />
-          Let's create something amazing!</span>
+          A passionate <span className="capitalize">{userKeywords[1]}</span>
+          <br />
+          <span className="mt-2 block italic">
+            Eager to bring ideas to life
+            <br />
+            Let's create something amazing!
+          </span>
         </motion.p>
         <motion.div
           initial={{ scale: 0 }}
@@ -49,7 +50,7 @@ const Home = () => {
         >
           <Link
             to="/projects"
-            className="rounded bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            className="rounded bg-teal-600 px-4 py-2 font-bold text-white hover:bg-teal-700"
           >
             View My Work
           </Link>
